6d5a8be0640d7b68ac3a917a3503fdd84aff11df
rkorytkowski <rafal.korytkowski@gmail.com>
2025-08-21 15:00:53 +0200

    Updating deps

--- helm/openmrs/helm_build.sh
+++ helm/openmrs/helm_build.sh
@@ -1,4 +1,7 @@
+helm dependency update ../openmrs-backend
 helm package ../openmrs-backend -d ../openmrs-backend/
+helm dependency update ../openmrs-frontend
 helm package ../openmrs-frontend -d ../openmrs-frontend/
+helm dependency update ../openmrs-gateway
 helm package ../openmrs-gateway -d ../openmrs-gateway/
 helm dependency update
